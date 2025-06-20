087945a14fc0fe8a2792a7e2a3bc8239e73a731d
VotreNom <VotreEmail@example.com>
2025-06-20 20:28:25 +0200

    Navbar+Footer

--- Argent-Bank/src/App.jsx
+++ Argent-Bank/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './assets/pages/HomePage.jsx';
+import NavBar from './assets/components/NavBar.jsx';
+import Footer from './assets/components/Footer.jsx';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
