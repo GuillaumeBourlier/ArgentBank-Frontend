07e0b3d303b9818a4e9c547ab24e3169bbd11364
VotreNom <VotreEmail@example.com>
2025-06-24 15:06:48 +0200

    SignInPage

--- Argent-Bank/src/App.jsx
+++ Argent-Bank/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import NavBar from "./components/NavBar.jsx";
 import Footer from "./components/Footer.jsx";
+import SignInPage from "./pages/SignInPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<SignInPage />} />
         </Routes>
         <Footer />
       </div>
